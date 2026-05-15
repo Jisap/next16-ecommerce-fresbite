a0dd11a8b8502e03c9b3e1ac197cc1f345806d86
Jorge Matos <jisapjisap@gmail.com>
2026-05-15 13:09:00 +0200

    feat: add Category page component with interactive Swiper carousel

--- app/UI-components/Pages/Index/Category/page.tsx
+++ app/UI-components/Pages/Index/Category/page.tsx
@@ -49,6 +49,44 @@ const Category = () => {
             Eating healthy starts at the grocery store.
           </p>
         </div>
+
+        <Swiper
+          slidesPerView={7}
+          spaceBetween={30}
+          loop={true}
+          modules={[Autoplay]}
+          autoplay={{
+            delay: 4000,
+          }}
+          speed={1500}
+          breakpoints={{
+            1600: { slidesPerView: 7 },
+            1400: { slidesPerView: 6 },
+            1200: { slidesPerView: 5 },
+            1000: { slidesPerView: 4.5 },
+            768: { slidesPerView: 3.5 },
+          }}
+        >
+          {categories.map((category, index) => (
+            <SwiperSlide key={index}>
+              <div className="category-wrap flex flex-col justify-center items-center cursor-pointer bg-gray-light rounded-md group h-62.5 px-4">
+                <div className="category-image bg-white shadow-md w-30 h-30 flex justify-center items-center rounded-full mb-2">
+                  <Image
+                    src={category.image}
+                    alt={category.title}
+                    className="transition-all duration-300 w-14 h-14"
+                  />
+                </div>
+
+                <div className="category-info my-2 flex flex-col justify-center items-center">
+                  <span className="text-xl text-center group-hover:text-prim transition-all duration-300 font-medium capitalize">
+                    {category.title}
+                  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   )
