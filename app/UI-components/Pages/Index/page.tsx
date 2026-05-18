daa429d9e5c94a17a68ab7180165a4b251fc29b1
Jorge Matos <jisapjisap@gmail.com>
2026-05-18 18:04:16 +0200

    feat: add CategoriesOffers component and integrate into the main index page

--- app/UI-components/Pages/Index/page.tsx
+++ app/UI-components/Pages/Index/page.tsx
@@ -4,6 +4,7 @@ import Category from './Category/page'
 import TopSelling from './TopSelling/page'
 import products from '@/app/JsonData/TopSelling.json'
 import Offers from './Offers/page'
+import CategoriesOffers from './CategoriesOffers/page'
 
 const Index = () => {
   return (
@@ -12,6 +13,7 @@ const Index = () => {
       <Category />
       <TopSelling product={products} />
       <Offers />
+      <CategoriesOffers />
     </>
   )
 }
