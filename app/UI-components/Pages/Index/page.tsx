4fca5b35051477b450d8fefec40373035ccf9781
Jorge Matos <jisapjisap@gmail.com>
2026-05-19 17:17:48 +0200

    feat: implement and integrate Testimonials component into index page structure

--- app/UI-components/Pages/Index/page.tsx
+++ app/UI-components/Pages/Index/page.tsx
@@ -11,6 +11,7 @@ import LatestProducts from './LatestProducts/page'
 import Order from './Order/page'
 import Brands from './Brands/page'
 import Services from './Services/page'
+import Testimonials from './Testimonials/page'
 
 const Index = () => {
   return (
@@ -26,6 +27,7 @@ const Index = () => {
       <Order />
       <Brands />
       <Services />
+      <Testimonials />
     </>
   )
 }
