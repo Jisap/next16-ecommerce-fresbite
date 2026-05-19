b776ecf7ec38533a5a8891ebcf5b0f17704da29b
Jorge Matos <jisapjisap@gmail.com>
2026-05-19 12:51:01 +0200

    feat: add BannerOffers component to display promotional product cards

--- app/UI-components/Pages/Index/BannerOffers/page.tsx
+++ app/UI-components/Pages/Index/BannerOffers/page.tsx
@@ -41,6 +41,66 @@ const BannerOffers = () => {
             </Link>
           </div>
         </div>
+
+        <div className="offer-banner relative w-full h-75 rounded-sm overflow-hidden">
+          <Image
+            src={banneroffer2}
+            alt="banneroffer1"
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute top-0 left-0 w-full h-full pt-16 pl-10">
+            <span className="text-white underline uppercase font-medium">
+              Get 49% off
+            </span>
+
+            <h3 className="text-4xl text-white max-w-5">
+              Freshly baked bread toast
+            </h3>
+
+            <Link
+              href="/UI-componentes/Pages/Shop"
+              className="bg-yellow-300 px-4 w-fit flex items-center py-2 mt-8 rounded-sm cursor-pointer hover:bg-black hover:text-white duration-300 transition-colors">
+              Shop Now
+              <Icon
+                icon="formkit:arrowright"
+                width="16"
+                height="16"
+                className="ms-2"
+              />
+            </Link>
+          </div>
+        </div>
+
+        <div className="offer-banner relative w-full h-75 rounded-sm overflow-hidden">
+          <Image
+            src={banneroffer3}
+            alt="banneroffer1"
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute top-0 left-0 w-full h-full pt-16 pl-10">
+            <span className="text-white underline uppercase font-medium">
+              Get 49% off
+            </span>
+
+            <h3 className="text-4xl text-white max-w-5">
+              Fruit and vegetables
+            </h3>
+
+            <Link
+              href="/UI-componentes/Pages/Shop"
+              className="bg-yellow-300 px-4 w-fit flex items-center py-2 mt-8 rounded-sm cursor-pointer hover:bg-black hover:text-white duration-300 transition-colors">
+              Shop Now
+              <Icon
+                icon="formkit:arrowright"
+                width="16"
+                height="16"
+                className="ms-2"
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   )
