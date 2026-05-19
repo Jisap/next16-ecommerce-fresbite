f9f08f9c2184ac006762297f8247189a14d8c08f
Jorge Matos <jisapjisap@gmail.com>
2026-05-19 16:29:12 +0200

    feat: add Order page component with responsive image layout

--- app/UI-components/Pages/Index/Order/page.tsx
+++ app/UI-components/Pages/Index/Order/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+"use client"
+
+import orderbanner1 from "@/public/order-image01.webp"
+import orderbanner2 from "@/public/order-image02.webp"
+import Image from "next/image"
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 px-2 lg:px-8 xl:px-12 py-8 sm:py-16 gap-5 lg:gap-8">
+        <div className="order-banner">
+          <Image
+            src={orderbanner1}
+            alt="orderbanner1"
+            className="w-full h-full"
+          />
+        </div>
+        <div>
+          <Image
+            src={orderbanner2}
+            alt="orderbanner2"
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
