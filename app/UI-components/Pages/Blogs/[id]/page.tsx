51ea0bff9fa757440ed26f10240dae54c0c3d3ca
Jorge Matos <jisapjisap@gmail.com>
2026-05-20 18:36:57 +0200

    feat: add BlogDetails component for dynamic blog content display and improved navigation

---
+++ app/UI-components/Pages/Blogs/[id]/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import sectionbanner from "@/public/section-banner.png"
+import ArticlesData from "@/app/JsonData/BlogsData.json"
+import { useParams } from 'next/navigation'
+
+
+const BlogDetails = () => {
+
+  const { id } = useParams();
+  const blog = ArticlesData.find((item) => item.id === Number(id));
+
+  if(!blog) return <div>Blog not found</div>
+
+  return (
+    <>
+      {/* Banner Principal */}
+      <div className="page-banner bg-black h-55 flex justify-between items-center relative">
+        <Image
+          src={sectionbanner}
+          alt="Section Banner"
+          fill
+          className="w-full h-full object-cover absolute top-0 left-0 right-0"
+        />
+
+        <div className="content z-10 w-full h-full flex justify-center items-center flex-col">
+          <ul className="flex items-center gap-1.5 bg-white/70 backdrop-blur-md px-4 py-1.5 rounded-full shadow-sm">
+            <li className="uppercase text-xs font-unbounded text-gray-800 hover:text-prim transition-colors">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="text-gray-500 font-bold">•</li>
+            <li className="uppercase text-xs font-unbounded text-prim font-semibold">
+              <Link href="/UI-components/Pages/Blogs">Blogs Details</Link>
+            </li>
+          </ul>
+
+          <h2 className="text-2xl sm:text-4xl font-unbounded font-bold text-black mt-3 drop-shadow-sm text-center px-4 max-w-3xl line-clamp-2">
+            {blog.title}
+          </h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BlogDetails
