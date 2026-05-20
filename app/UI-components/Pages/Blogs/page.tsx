b68688cd0e2692a202f1b60336c0c86434a1dc26
Jorge Matos <jisapjisap@gmail.com>
2026-05-20 17:40:39 +0200

    feat: implement Blogs page with banner and navigation links for improved user experience

--- app/UI-components/Pages/Blogs/page.tsx
+++ app/UI-components/Pages/Blogs/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import sectionbanner from "@/public/section-banner.png"
+
+const Blogs = () => {
   return (
-    <div>page</div>
+    <>
+      {/* Banner Principal */}
+      <div className="page-banner bg-black h-55 flex justify-between items-center relative">
+        <Image
+          src={sectionbanner}
+          alt="Section Banner"
+          fill
+          className="w-full h-full object-cover absolute top-0 left-0 right-0"
+        />
+
+        <div className="content z-10 w-full h-full flex justify-center items-center flex-col">
+          <ul className="flex items-center gap-1.5 bg-white/70 backdrop-blur-md px-4 py-1.5 rounded-full shadow-sm">
+            <li className="uppercase text-xs font-unbounded text-gray-800 hover:text-prim transition-colors">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="text-gray-500 font-bold">•</li>
+            <li className="uppercase text-xs font-unbounded text-prim font-semibold">
+              <Link href="/UI-components/Pages/Shop">Blogs</Link>
+            </li>
+          </ul>
+
+          <h2 className="text-2xl sm:text-4xl font-unbounded font-bold text-black mt-3 drop-shadow-sm">
+            Our Articles
+          </h2>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Blogs
