ac59618b55b69784b2e5588f36d83c1595e6774b
Jorge Matos <jisapjisap@gmail.com>
2026-05-23 16:15:06 +0200

    feat: add PageNotFound component with banner and breadcrumb navigation

--- app/UI-components/Pages/PageNotFound/page.tsx
+++ app/UI-components/Pages/PageNotFound/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { EntranceAnimation } from '@/app/Animations'
+import Image from 'next/image'
+import Link from 'next/link'
+import sectionbanner from "@/public/section-banner.png";
 
-const page = () => {
+const PageNotFound = () => {
   return (
-    <div>page</div>
+    <>
+      {/* Banner Principal */}
+      <div className="page-banner bg-black h-55 flex justify-between items-center relative">
+        <Image
+          src={sectionbanner}
+          alt="Section Banner"
+          fill
+          className="w-full h-full object-cover absolute top-0 left-0 right-0"
+        />
+
+        <EntranceAnimation type="fadeDown" duration={0.8} scrollTrigger={false} className="content z-10 w-full h-full flex justify-center items-center flex-col">
+          <ul className="flex items-center gap-1.5 bg-white/70 backdrop-blur-md px-4 py-1.5 rounded-full shadow-sm">
+            <li className="uppercase text-xs font-unbounded text-gray-800 hover:text-prim transition-colors">
+              <Link href="/">Home</Link>
+            </li>
+
+            <li className="text-gray-500 font-bold">•</li>
+
+            <li className="uppercase text-xs font-unbounded text-prim font-semibold">
+              <Link href="/UI-components/Pages/Blogs">Blogs</Link>
+            </li>
+          </ul>
+
+          <h2 className="text-2xl sm:text-4xl font-unbounded font-bold text-black mt-3 drop-shadow-sm">
+            Our Articles
+          </h2>
+        </EntranceAnimation>
+      </div>
+    </>
   )
 }
 
-export default page
+export default PageNotFound
